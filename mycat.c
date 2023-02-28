9a762f1adee356bbdbf14d33aaa6bc3b314c0991
Samba.lnv <openwac.samba@gmail.com>
2023-02-28 10:39:51 +0000

    Cenas

--- mycat.c
+++ mycat.c
@@ -2,13 +2,46 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define size 1000
-
-int main ()
+typedef struct
 {
-    char buf [size];
-    int bytes;
-    while ((bytes= read (STDIN_FILENO, buf, size))> 0)
-        write (STDOUT_FILENO, buf, bytes);
+    int pos;
+    int size;
+    char* str;
+}string;
+
+string* init_string ()
+{
+    string* s= malloc (sizeof (string));
+    s->pos= 0;
+    s->size= 100;
+    s->str= malloc (100* sizeof (char));
+    return s;
+}
+
+void more_string (string* s)
+{
+    s->size<<= 1;
+    s->str= realloc (s->str, s->size* sizeof(char));
+}
+
+void free_string (string* s)
+{
+    free (s->str);
+    free (s);
+}
+
+int main (int argc, const char* argv[])
+{
+    string* s= init_string ();
+    while (fgets (&s->str[s->pos], 100, stdin))
+    {
+        s->pos+= strlen (&s->str[s->pos]);
+        if (s->pos- strlen(s->str)<100)
+            more_string (s);
+    }
+    puts (s->str);
+    free_string (s);
 }
